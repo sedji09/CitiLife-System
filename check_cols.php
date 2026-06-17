676ac8d04283b41af8435d9a662c940d7aa01479
sedji09 <seigipascual09@gmail.com>
2026-06-17 22:58:47 +0800

    nagdelete ng ibang files

--- check_cols.php
+++
@@ -1,4 +0,0 @@
-<?php
-require 'config/database.php';
-$stmt = $pdo->query("SHOW COLUMNS FROM users");
-print_r($stmt->fetchAll(PDO::FETCH_COLUMN));
--- list_users.php
+++
@@ -1,4 +0,0 @@
-<?php
-require 'config/database.php';
-$stmt = $pdo->query('SELECT id, email, name, role FROM users');
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
